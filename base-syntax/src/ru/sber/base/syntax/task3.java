a5394a7718af01b1c7271093f9e771cf96754c5e
AstrovAlice <astrovalice@gmail.com>
2024-02-20 01:49:21 +0300

    Initial commit

---
+++ base-syntax/src/ru/sber/base/syntax/task3.java
@@ -0,0 +1,13 @@
+package ru.sber.base.syntax;
+
+public class task3 {
+    public static void main(String[] args){
+        for (int i = 1; i <= 6; i++) {
+            if ((i % 2) != 0){
+                System.out.println("* * * * * * * * * *");
+            }else{
+                System.out.println(" * * * * * * * * * ");
+            }
+        }
+    }
+}
