e489ccc54906225289cc1a7e9b9907d52489f5a2
AstrovAlice <astrovalice@gmail.com>
2024-02-20 09:30:03 +0300

    Initial commit

--- base-syntax/src/ru/sber/base/syntax/Task10.java
+++ base-syntax/src/ru/sber/base/syntax/Task10.java
@@ -1,6 +1,6 @@
 package ru.sber.base.syntax;
 public class Task10 {
     public static void main(String[] args){
-
+        javax.swing.JOptionPane.showMessageDialog(null, "Привет, Эдуард!");
     }
 }
