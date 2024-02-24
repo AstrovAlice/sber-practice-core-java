5b02a4542fced6bbc11c0dd32aaa9836eecc2005
AstrovAlice <astrovalice@gmail.com>
2024-02-24 16:09:55 +0300

    Initial commit

--- base-syntax/src/ru/sber/base/syntax/Week2/Task9.java
+++ base-syntax/src/ru/sber/base/syntax/Week2/Task9.java
@@ -1,7 +1,27 @@
 package ru.sber.base.syntax.Week2;
+import java.util.Random;
+import java.util.Scanner;
 public class Task9 {
     public static void main(String[] args){
-        System.out.print("Проверка");
-
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        char randChar = (char) (random.nextInt(26) + 'A');
+        System.out.println("Загадана буква от A до Z.");
+        System.out.println("Попробуйте её угадать.");
+        char ch = 'И';
+        do{
+            System.out.println("Вариант ответа: ");
+            ch = scanner.next().charAt(0);
+            while((ch <= 'A') || ('Z' <= ch)){
+                System.out.print("");
+                ch = scanner.next().charAt(0);
+            }
+            if (ch > randChar){
+                System.out.print("Ищи букву, котоаря перед ней в алфавите.");
+            } else if (ch < randChar){
+                System.out.print("Ищи букву, котоаря после неё в алфавите.");
+            }
+        }while(ch != randChar);
+        System.out.print("Верно!");
     }
 }
