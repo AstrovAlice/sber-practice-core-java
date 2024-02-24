a2e3fa67cd5fa347db0f19c73720c28a90fb9770
AstrovAlice <astrovalice@gmail.com>
2024-02-24 16:10:56 +0300

    Initial commit

--- base-syntax/src/ru/sber/base/syntax/Week2/Task8.java
+++ base-syntax/src/ru/sber/base/syntax/Week2/Task8.java
@@ -6,7 +6,6 @@ public class Task8 {
     public static void main(String[] args){
         int n = 30;
         int nmax = 1000;
-
         Random random = new Random();
         int[] a = new int[n];
         for(int i = 0; i < n; i++){
