18fbb74a91097f6c9760fbba611b12feb3b98d33
AstrovAlice <astrovalice@gmail.com>
2024-03-09 15:09:36 +0300

    Initial commit

--- base-syntax/src/Week2/Task9.java
+++ base-syntax/src/Week2/Task9.java
@@ -1,30 +1,34 @@
 package Week2;
+
 import java.util.Random;
 import java.util.Scanner;
+
 public class Task9 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         char randChar = (char) (random.nextInt(26) + 'A');
+
         System.out.println("Загадана буква от A до Z.");
         System.out.println("Попробуйте её угадать.");
-        char ch = 'И';
-        do{
+
+        char ch;
+        do {
             System.out.print("Вариант ответа: ");
-            ch = scanner.next().charAt(0);
-            System.out.print("");
-            while((ch < 'A') || ('Z' < ch)){
-                System.out.println("Вводите букву от A до Z!");
+            ch = Character.toUpperCase(scanner.next().charAt(0));
+
+            while (ch < 'A' || ch > 'Z') {
+                System.out.println("Введите букву от A до Z!");
                 System.out.print("Вариант ответа: ");
-                ch = scanner.next().charAt(0);
-                System.out.print("");
+                ch = Character.toUpperCase(scanner.next().charAt(0));
             }
-            if (ch > randChar){
-                System.out.println("Ищи букву, котоаря перед ней в алфавите.");
-            } else if (ch < randChar){
-                System.out.println("Ищи букву, котоаря после неё в алфавите.");
+            if (ch > randChar) {
+                System.out.println("Ищи букву, которая перед ней в алфавите.");
+            } else if (ch < randChar) {
+                System.out.println("Ищи букву, которая после неё в алфавите.");
             }
-        }while(ch != randChar);
+        } while (ch != randChar);
+
         System.out.print("Верно!");
     }
 }
