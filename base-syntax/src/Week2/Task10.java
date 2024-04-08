a05faf28c2ef3f0275471909581eac138dbd5187
AstrovAlice <astrovalice@gmail.com>
2024-04-08 21:53:18 +0300

    Initial commit

--- base-syntax/src/Week2/Task10.java
+++ base-syntax/src/Week2/Task10.java
@@ -1,6 +1,6 @@
 package Week2;
 public class Task10 {
     public static void main(String[] args){
-        javax.swing.JOptionPane.showMessageDialog(null, "Привет, Эдуард! ");
+        javax.swing.JOptionPane.showMessageDialog(null, "Привет, Эдуард!");
     }
 }
